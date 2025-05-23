85a2a81e871ed978bf5bdef88ce880ebd496eec2
JesuQM <jesusdavidqmq@gmail.com>
2025-05-23 17:10:04 -0500

    se copleta el crud de django

--- contactos_app/models.py
+++ contactos_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contacto(models.Model):
+    nombre=models.CharField(max_length=100,null=False,blank=False,verbose_name='Nombre')
+    apellido=models.CharField(max_length=100,null=False,blank=False,verbose_name='Apellido')
+    email= models.EmailField(max_length=100,null=False,blank=False,verbose_name='Email')
+    telefono=models.CharField(max_length=15,null=False,blank=False,unique=True,verbose_name='Telefono')
+    fotografia=models.ImageField(upload_to='fotos',default='fotos/default.jpg',null=True,blank=True,verbose_name='Fotografia')
+    
+
--- contactos_app/views.py
+++ contactos_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Contacto
+from .serializers import ContactoSerializer
+from rest_framework.exceptions import ValidationError
 
-# Create your views here.
+class ContactoViewSet(viewsets.ModelViewSet):
+    queryset = Contacto.objects.all()
+    serializer_class = ContactoSerializer
+        
